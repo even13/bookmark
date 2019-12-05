7dbad53acfa6e0ee4186a133026596644ab8dd97
Eve Noirault <student@Makerss-MacBook-Air.local>
2019-12-05 11:58:11 +0000

    refactor tests

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,7 +2,7 @@ require 'bookmark'
 
 describe Bookmark do
 
-  describe "#all" do
+  describe ".all" do
 
     it "should display all my bookmarks" do
       clear_database
@@ -15,6 +15,15 @@ describe Bookmark do
       expect(bookmarks).to include("http://www.makersacademy.com")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")
+    end
+  end
+
+  describe '.create' do
+    it 'creates a new bookmark' do
+      clear_database
+      Bookmark.create("http://www.testbookmark.com")
+
+      expect(Bookmark.all).to include("http://www.testbookmark.com")
     end
   end
 
